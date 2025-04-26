79455958566a14271a8ca0829b8218ffcc4e5cb1
FalconCharge <99128705+FalconCharge@users.noreply.github.com>
2025-04-25 21:06:52 -0300

    Added different cargo to spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,11 +1,13 @@
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] products;
-    [SerializeField] private float timer = 1f;
+    [SerializeField] private float timer = 5f;
+
+    [SerializeField] private float minOffset = -5f;
+    [SerializeField] private float maxOffset = 5f;
 
     private CountDownTimer countDownTimer;
 
@@ -19,8 +21,25 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         if(countDownTimer.IsReady()){
-            Instantiate(products[0], transform.position, quaternion.identity);
+            SpawnRandomProduct();
             countDownTimer.Reset();
         }
+    }
+
+    private void SpawnRandomProduct()
+    {
+        if (products == null || products.Length == 0)
+        {
+            Debug.LogWarning("[Spawner] No products assigned!");
+            return;
+        }
+
+        GameObject prefab = products[Random.Range(0, products.Length)];
+
+        float xOff = Random.Range(minOffset, maxOffset);
+        float zOff = Random.Range(minOffset, maxOffset);
+        Vector3 spawnPos = transform.position + new Vector3(xOff, 0f, zOff);
+
+        Instantiate(prefab, spawnPos, Quaternion.identity);
     }
 }
