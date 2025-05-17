0a9cb229ba25d362c731f17447606d013f56d18c
FalconCharge <99128705+FalconCharge@users.noreply.github.com>
2025-05-16 21:25:16 -0300

    Added progression bar

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] Slider progressionBar;
     [SerializeField] private float currTime = 1000f;
+
+    private float maxTime;
 
     private bool isDone = false;
 
@@ -13,29 +17,50 @@ public class GameTimer : MonoBehaviour
         Timer();
     }
 
-    private void Timer(){
+    private void Timer()
+    {
 
-        if(!isDone){
-            if(currTime <= 0){
+        if (!isDone)
+        {
+            if (currTime <= 0)
+            {
                 currTime = 0f;
                 isDone = true;
-            }else{
+            }
+            else
+            {
                 currTime -= Time.deltaTime;
             }
-            UpdateText();
+            UpdateUI();
         }
     }
 
-    public void StartTimer(float duration){
+    public void StartTimer(float duration)
+    {
         isDone = false;
+        maxTime = duration;
         currTime = duration;
+
+        progressionBar.maxValue = duration;
+        progressionBar.value = maxTime - currTime;
+
+
+        UpdateUI();
     }
 
-    public bool IsDone(){
+    public bool IsDone()
+    {
         return isDone;
     }
 
-    private void UpdateText(){
+    private void UpdateUI()
+    {
+        progressionBar.value = maxTime - currTime;
         timerText.text = "Timer: " + currTime.ToString("F2");
+    }
+
+    public float GetCurrentTime()
+    {
+        return currTime;
     }
 }
